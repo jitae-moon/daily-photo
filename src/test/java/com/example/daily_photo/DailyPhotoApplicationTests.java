8cb0198d82f708fd9098daaa9d80526437b4ee29
jitae-moon <jitae.moon@icloud.com>
2024-06-20 16:05:21 +0900

    feat #2 - Add basic files for project setup

---
+++ src/test/java/com/example/daily_photo/DailyPhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daily_photo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyPhotoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
